b4dd553c726964f95bac15ae7a361e103dd61096
suspect_x <85556765+Levea-G@users.noreply.github.com>
2023-05-17 21:06:57 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ def receive_msg(client_socket,chats):
             chats.record.config(state='disabled')
         except:
             chats.record.config(state='normal')
-            chats.record.insert('end','Disconnected from the server')
+            chats.record.insert('end','Disconnected from server\n')
             chats.record.see('end')
             chats.record.config(state='disabled')
             break
@@ -31,6 +31,18 @@ class chat():
             self.msg.delete(0.0,'end')
         def enter(event=None):
             self.msg.insert(self.msg.index('insert'),'')
+        def reconnect():
+            global client_socket
+            msg=''
+            try:
+                client_socket.connect((Host,Port))
+                msg='Connected to %s:%d\n\n'%(Host,Port)
+                threading.Thread(target=receive_msg,args=(client_socket,self),daemon=True).start()
+            except:msg='Reconnect to %s:%d failed\n\n'%(Host,Port)
+            self.record.config(state='normal')
+            self.record.insert('end',msg)
+            self.record.see('end')
+            self.record.config(state='disabled')
         self.main=tk.Tk()
         self.main.title('chatroom')
         self.main.geometry('640x480+300+200')
@@ -45,16 +57,10 @@ class chat():
         sc2.pack(side='right',fill='y',in_=self.msg,expand=0)
         self.msg.config(yscrollcommand=sc2.set)
         tk.Button(self.main,font=('times',12),text='clear\nrecord',command=clear).place(relx=0.05,rely=0.2,relwidth=0.12,relheight=0.1)
+        tk.Button(self.main,font=('times',12),text='reconnect',command=reconnect).place(relx=0.05,rely=0.6,relwidth=0.1,relheight=0.1)
         self.main.bind('<Return>',send)
         self.main.bind('<Alt-KeyPress-Return>',enter)
-        self.msg.focus()
+        self.msg.focus();reconnect()
 if __name__=='__main__':
     client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client_socket.connect((Host,Port))
-    temp=chat()
-    temp.record.config(state='normal')
-    temp.record.insert('end','Connected to %s:%d\n\n'%(Host,Port))
-    temp.record.see('end')
-    temp.record.config(state='disabled')
-    threading.Thread(target=receive_msg,args=(client_socket,temp),daemon=True).start()
-    temp.main.mainloop()
+    chat().main.mainloop()
